824762875174424ced91237ef916ddce4de2a631
qrevalenzuela <112681441+qrevalenzuela@users.noreply.github.com>
2023-12-14 04:14:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const connection = require('./db')
+const path = require('path');
+const connection = require('./db');
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 const quoteRoutes = require('./routes/quotes');
-const path = require('path')
 
-// database connection
-connection()
+const app = express();
 
-// middlewares
+// Database connection
+connection();
+
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// routes
+// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/quote', quoteRoutes);
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.use("*",function(req, res){
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+// Catch-all route for serving React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
+
+// Error handling
+app.on('error', (error) => {
+  console.error('Express server error:', error.message);
+  process.exit(1); // Exit the process on error for better visibility
+});
