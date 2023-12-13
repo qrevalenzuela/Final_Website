b02b0a8576bd9c30e87175b06e14855dc17fa969
qrevalenzuela <112681441+qrevalenzuela@users.noreply.github.com>
2023-12-14 03:44:33 +0800

    Final build

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connection = require('./db')
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+const quoteRoutes = require('./routes/quotes');
+const path = require('path')
+
+// database connection
+connection()
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/quote', quoteRoutes);
+
+app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}...`))
